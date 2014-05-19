7c07b38434e5b1cda784bd348e9521f8ae0b0733
tom-englert <mail@tom-englert.de>
2014-05-19 11:40:46 +0000

    Add group style binding helper class
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS36]$/DataGridExtensions;C31664

---
+++ DataGridExtensions/GroupStyleBinding.cs
@@ -0,0 +1,62 @@
+﻿namespace DataGridExtensions
+{
+    using System.Collections;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Helper class to support binding to the group style from within a style setter.
+    /// </summary>
+    public static class GroupStyleBinding
+    {
+        /// <summary>
+        /// Gets the group style.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns>The group style.</returns>
+        public static GroupStyle GetGroupStyle(DependencyObject obj)
+        {
+            return (GroupStyle)obj.GetValue(GroupStyleProperty);
+        }
+        /// <summary>
+        /// Sets the group style.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <param name="value">The value.</param>
+        public static void SetGroupStyle(DependencyObject obj, GroupStyle value)
+        {
+            obj.SetValue(GroupStyleProperty, value);
+        }
+        /// <summary>
+        /// Defines the group style dependency property.
+        /// </summary>
+        public static readonly DependencyProperty GroupStyleProperty =
+            DependencyProperty.RegisterAttached("GroupStyle", typeof(GroupStyle), typeof(GroupStyleBinding), new FrameworkPropertyMetadata(GroupStyle_Changed));
+
+        private static void GroupStyle_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var itemsControl = d as ItemsControl;
+            if (itemsControl == null)
+                return;
+
+            itemsControl.GroupStyle.Clear();
+
+            var groupStyle = e.NewValue as GroupStyle;
+            if (groupStyle != null)
+            {
+                itemsControl.GroupStyle.Add(groupStyle);
+                return;
+            }
+
+            var groupStyles = e.NewValue as IEnumerable;
+            if (groupStyles != null)
+            {
+                foreach (var item in groupStyles.OfType<GroupStyle>())
+                {
+                    itemsControl.GroupStyle.Add(item);
+                }
+            }
+        }
+    }
+}
