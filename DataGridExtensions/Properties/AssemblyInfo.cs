f2a006315e6743c3e4249c9bcfeee08a642387f0
tom-englert <mail@tom-englert.de>
2015-04-26 16:11:37 +0000

    Prepare next version
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS36]$/DataGridExtensions;C35347

--- DataGridExtensions/Properties/AssemblyInfo.cs
+++ DataGridExtensions/Properties/AssemblyInfo.cs
@@ -20,6 +20,6 @@ using System.Windows.Markup;
 [assembly: XmlnsDefinition("urn:tom-englert.de/DataGridExtensions", "DataGridExtensions")]
 [assembly: XmlnsDefinition("urn:tom-englert.de/DataGridExtensions", "DataGridExtensions.Behaviors")]
 
-[assembly: AssemblyVersion("1.0.22.0")]
-[assembly: AssemblyFileVersion("1.0.22.0")]
+[assembly: AssemblyVersion("1.0.23.0")]
+[assembly: AssemblyFileVersion("1.0.23.0")]
 
