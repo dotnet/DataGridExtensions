61d7131106da04574546f377acb639966dd7a5c7
tom-englert <mail@tom-englert.de>
2016-02-02 10:29:59 +0000

    Sample for a simple combo box filter
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS36]$/DataGridExtensions;C36946

--- DataGridExtensionsSample/DataItem.cs
+++ DataGridExtensionsSample/DataItem.cs
@@ -7,6 +7,8 @@ namespace DataGridExtensionsSample
 {
     public class DataItem
     {
+        private static string[] _samples = new[] {"lorem", "ipsum", "dolor", "sit", "amet"};
+
         public DataItem(Random rand, int index)
         {
             Flag = rand.Next(2) == 0;
@@ -15,6 +17,7 @@ namespace DataGridExtensionsSample
             Column2 = rand.Next(20) == 0 ? null : Guid.NewGuid().ToString();
             Column3 = Guid.NewGuid().ToString();
             Column4 = Guid.NewGuid().ToString();
+            Column5 = _samples[rand.Next(_samples.Length)];
             Probability = rand.NextDouble();
         }
 
@@ -24,6 +27,7 @@ namespace DataGridExtensionsSample
         public string Column2 { get; set; }
         public string Column3 { get; set; }
         public string Column4 { get; set; }
+        public string Column5 { get; set; }
         public double Probability { get; private set; }
     }
 }
