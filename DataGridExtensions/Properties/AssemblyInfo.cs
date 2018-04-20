0432e29dc168c032ce2016ffe7faa36c49cd50e8
tom-englert <mail@tom-englert.de>
2018-04-20 12:52:25 +0200

    Move assembly meta data to project and Directory.Build.props

--- DataGridExtensions/Properties/AssemblyInfo.cs
+++ DataGridExtensions/Properties/AssemblyInfo.cs
@@ -4,29 +4,13 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
 
-[assembly: AssemblyTitle("Data Grid Extensions")]
-[assembly: AssemblyDescription("This package contains useful extensions for the MS DataGrid (System.Windows.Controls.DataGrid).\r\nThe current version supports the following features:\r\n- Add filtering capabilities to the DataGrid.\r\n- Correctly apply initial sorting criteria.\r\n- Easy to use\r\n- Easy to customize\r\n- Attaches to the existing DataGrid")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("tom-englert.de")]
-[assembly: AssemblyProduct("Tom's Toolbox")]
-[assembly: AssemblyCopyright("Copyright © 2012-2018 tom-englert.de, Germany")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
-[assembly: Guid("26c621e5-4dac-4f34-b534-9bb334d6380b")]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 [assembly: XmlnsPrefix("urn:tom-englert.de/DataGridExtensions", "dgx")]
 [assembly: XmlnsDefinition("urn:tom-englert.de/DataGridExtensions", "DataGridExtensions")]
 [assembly: XmlnsDefinition("urn:tom-englert.de/DataGridExtensions", "DataGridExtensions.Behaviors")]
 
-[assembly: AssemblyVersion(Product.Version)]
-[assembly: AssemblyFileVersion(Product.Version)]
-
-// ReSharper disable once CheckNamespace
-internal static class Product
-{
-    public const string Version = "1.0.37.0";
-}
 
