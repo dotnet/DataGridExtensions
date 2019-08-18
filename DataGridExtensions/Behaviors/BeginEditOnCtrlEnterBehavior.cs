04e1e4f7eb4e55c27e5b7c75595591939bf13c4a
Marcel Juen <marcel.juen@raincoat.systems>
2019-08-18 12:46:56 +0200

    Add .NET Core 3.0 target and change dependencies accordingly

--- DataGridExtensions/Behaviors/BeginEditOnCtrlEnterBehavior.cs
+++ DataGridExtensions/Behaviors/BeginEditOnCtrlEnterBehavior.cs
@@ -4,7 +4,11 @@
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Input;
+#if NETFRAMEWORK
     using System.Windows.Interactivity;
+# elif NETCOREAPP
+    using Microsoft.Xaml.Behaviors;
+#endif
 
     using JetBrains.Annotations;
 
