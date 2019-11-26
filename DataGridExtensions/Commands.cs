c8128993b39030e0c373623986d88c973cd63207
jkuehner <jochen.kuehner@kardex.com>
2019-11-26 14:43:47 +0100

    Clearable combobox

---
+++ DataGridExtensions/Commands.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Input;
+using Microsoft.Xaml.Behaviors.Core;
+using System.Windows.Controls;
+
+namespace DataGridExtensions
+{
+    public static class Commands
+    {
+       public static ICommand ClearComboBoxCommand => new ActionCommand((control) =>
+           {
+               ((ComboBox) control).SelectedItem = null;
+           });
+    }
+}
