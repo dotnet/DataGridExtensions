7b91013da532166d93e755a2e1b916aaf0d1d539
tom-englert <mail@tom-englert.de>
2020-09-05 14:33:15 +0200

    Style sample app

--- src/DataGridExtensionsSample/App.xaml.cs
+++ src/DataGridExtensionsSample/App.xaml.cs
@@ -1,9 +1,19 @@
 ﻿namespace DataGridExtensionsSample
 {
+    using System.Windows;
+    using TomsToolbox.Wpf.Styles;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Resources.MergedDictionaries.Add(WpfStyles.GetDefaultStyles());
+            Resources.RegisterDefaultWindowStyle();
+        }
     }
 }
