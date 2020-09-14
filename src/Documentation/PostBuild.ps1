8d31f8ea73c3b675e903828c439ad7997b0b192c
tom-englert <mail@tom-englert.de>
2020-09-14 19:49:37 +0200

    Add API Documentation

---
+++ src/Documentation/PostBuild.ps1
@@ -0,0 +1,17 @@
+
+function Replace-Version
+{
+    [cmdletbinding()]
+    param (
+        [Parameter(Position=0, Mandatory=1, ValueFromPipeline=$true)]
+        [string]$text
+    )
+
+    return $text -replace ' Version: \d\.\d\.\d\+[a-z0-9]+', ''
+
+}
+
+Get-ChildItem '..\..\doc\html\*.htm' | ForEach {
+(Get-Content $_ -Raw | Replace-Version) | Set-Content -Path $_ -Encoding UTF8
+}
+
